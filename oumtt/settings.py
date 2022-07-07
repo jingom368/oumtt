24c6807b47caccf24160f91d64641ba8bb4aa27d
jignom368 <jingom369@gmail.com>
2022-07-07 12:20:12 +0900

    update

--- oumtt/settings.py
+++ oumtt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-46*#tn@6@b#&v-(8z#pd6mas^#pr5_w@#nf!x%73wgu-pv7d11
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = './static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
