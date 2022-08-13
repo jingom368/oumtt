cd61840e5b31fc6ce5f67fea8dbca3315b457c55
jignom368 <jingom369@gmail.com>
2022-08-13 21:54:01 +0900

    update

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.getRoutes, name='routes'),
+    path('reservations/', views.getReservations, name='reservations'),
+    # path('notes/create/', views.createNote, name='create-note'),
+    # path('notes/<str:pk>/update/', views.updateNote, name='update-note'),
+    # path('notes/<str:pk>/delete/', views.deleteNote, name='delete-note'),
+
+    path('reservations/<str:pk>/', views.getReservation, name='reservation'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Reservation
+from .serializers import ReservationSerializer
+from api import serializers
+# from .utils import getNoteList, createNote, getNotedetail, updateNote, deleteNote
 
-# Create your views here.
+@api_view(['GET']) 
+def getRoutes(request):
+
+    routes = [
+        {
+            'Endpoint': '/reservations/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/reservations/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single reservation object'
+        },
+        {
+            'Endpoint': '/reservations/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new reservation with data sent in post request'
+        },
+        {
+            'Endpoint': '/reservations/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing reservation with data sent in post request'
+        },
+        {
+            'Endpoint': '/reservations/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting reservation'
+        },
+    ]
+    return Response(routes)
+
+# /notes GET
+# /notes POST
+# /notes/<id> GET
+# /notes/<id> PUT
+# /notes/<id> DELETE
+
+@api_view(['GET', 'POST'])
+def getReservations(request):
+
+    if request.method == 'GET':
+        reservations = Reservation.objects.all()
+        serializer = ReservationSerializer(reservations, many=True)
+        return Response(serializer.data)
+        # return getNoteList(request)
+
+#     if request.method == 'POST':
+#         return createNote(request)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def getReservation(request, pk):
+
+    if request.method == 'GET':
+        reservations = Reservation.objects.get(id=pk)
+        serializer = ReservationSerializer(reservations, many=False)
+        return Response(serializer.data)
+        # return getNotedetail(request, pk)
+
+#     if request.method == 'PUT':
+#         return updateNote(request, pk)
+
+#     if request.method == 'DELETE':
+#         return deleteNote(request, pk)
